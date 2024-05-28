c97ea853b7eb7a8c8b7080ea19f6a511cda34473
shohagnawaz <snawazdigital@gmail.com>
2024-05-28 18:13:24 +0600

    end

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 // middleware
@@ -24,10 +24,44 @@ async function run() {
     await client.connect();
 
     const userCollection = client.db('userDataBase').collection('users');
+    app.get('/users', async(req, res) => {
+        const cursor = userCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    });
+    app.get('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };  
+        const user = await userCollection.findOne(query);
+        res.send(user);
+
+    });
+    app.put('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const user = req.body;
+        console.log(id, user);
+        const filter = { _id: new ObjectId(id) };
+        const options = { upsert: true };
+        const updatedUser = {
+          $set: {
+              name: user.name,
+              email: user.email
+          }
+        }
+        const result = await userCollection.updateOne(filter, updatedUser, options);
+        res.send(result);
+    });
     app.post('/users', async(req, res) => {
         const user = req.body;
         console.log('new user', user);
         const result = await userCollection.insertOne(user);
+        res.send(result);
+    });
+    app.delete('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        console.log('please delete from database', id);
+        const query = { _id: new ObjectId(id) };
+        const result = await userCollection.deleteOne(query);
         res.send(result);
     });
 
